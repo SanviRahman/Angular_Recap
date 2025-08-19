f0b90a3684f4660486b2254f2344a98e3027feb6
Sanvi Rahman <SanviRahman>
2025-08-19 14:59:14 +0600

    Done

---
+++ Component_LifeCycle/src/app/comp1/comp1.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-comp1',
+  templateUrl: './comp1.component.html',
+  styleUrls: ['./comp1.component.css']
+})
+export class Comp1Component {
+  @Input() value: string ='';
+
+  constructor() {
+    console.log("Constructor Called");
+  }
+
+  ngOnChanges(change: any) {
+    console.log('ngOnChanges Called');
+    console.log(change);
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit Called');
+  }
+
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit Called');
+  }
+
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy Called');
+  }
+}
